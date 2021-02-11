aaf543095297a9ed8e16bdf8d8f3bedc8cfcf3e5
Simon McCorkindale <simon@funx.jp>
2021-02-11 15:32:39 +0800

    Change admin view example wording

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -1,9 +1,9 @@
 <div class="container-fluid">
     <div class="panel panel-default">
-        <div class="panel-heading"><strong>Example-basic</strong> <?= Yii::t('ExampleBasicModule.base', 'configuration') ?></div>
+        <div class="panel-heading"><strong>Souls admin</strong> <?= Yii::t('SoulsModule.base', 'configuration') ?></div>
 
         <div class="panel-body">
-            <p><?= Yii::t('ExampleBasicModule.base', 'Welcome to the admin only area.') ?></p>
+            <p><?= Yii::t('SoulsModule.base', 'Welcome to the admin only area.') ?></p>
         </div>
     </div>
 </div>
