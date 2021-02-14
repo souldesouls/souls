8220034a91c91178d47f289893d6ced834fdb58c
Simon McCorkindale <simon@funx.jp>
2021-02-14 12:21:26 +0800

    Use localize mechanism

--- views/matcher/matcher.php
+++ views/matcher/matcher.php
@@ -8,17 +8,18 @@ use yii\helpers\Url;
 <div class="container">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <i class="fa fa-users"></i> <strong><?= Yii::t('SoulsModule.base', 'SOULS MATCHER') ?></strong>
+            <i class="fa fa-users"></i> <strong><?= Yii::t('SoulsModule.views.matcher', 'SOULS MATCHER') ?></strong>
             <hr>
         </div>
         <div class="panel-body">
             <center>
+
                 <p class="lead">
-                    Matching begins with intention.
+                    <?= Yii::t('SoulsModule.base', 'Matching begins with intention.') ?>
                 </p>
 
                 <?= Button::primary(Yii::t('SoulsModule.views.matcher', 'BEGIN'))->link(Url::toRoute(['set-intention'])); ?>
-                
+
             </center>
         </div>
     </div>
