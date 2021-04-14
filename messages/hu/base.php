f9ca54346fa64d266dd3a50a27ea32f79c25fb97
Souls App <soulspear@souls.energy>
2021-04-14 10:09:43 +0000

    Add messages folder from yii message/extract-module

---
+++ messages/hu/base.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Guest' => '',
+    'Hello Topics World!' => '',
+    'Hi there {name}!' => '',
+    'Matcher' => '',
+    'Matching begins with intention.' => '',
+    'Say Hello!' => '',
+    'Souls' => '',
+    'Welcome to the admin only area.' => '',
+    'configuration' => '',
+    'overview' => '',
+];
---
+++ messages/nb-NO/views.matcher.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'A <strong>{percent}%</strong> match was found.' => '',
+    'A <strong>{percent}%</strong> match was found. Try again:' => '',
+    'BEGIN' => '',
+    'FIND A MATCH!' => '',
+    'Focus those thoughts into your intent as you press the button below. The system will try and measure your intent and match you with someone.' => '',
+    'Give some thought to that reason, and try and imagine what kind of person they may be.' => '',
+    'Hello, nice to meet you!' => '',
+    'MATCH RESULTS' => '',
+    'Or just start chatting!' => '',
+    'SET YOUR INTENT' => '',
+    'SOULS MATCHER' => '',
+    'START CHATTING' => '',
+    'SUGGESTED TOPIC CHAT' => '',
+    'Start chatting with a topic suggestion that\'s based on both your measurements.' => '',
+    'The lines show your measured intention alongside the closest matching member. Matches range from -100% to 100% and are based on how similar both of your measurements are.' => '',
+    'What\'s the reason you are looking for someone? (Anything goes here:)' => '',
+    'You two have a {percent}% match. Happy chatting!' => '',
+];
